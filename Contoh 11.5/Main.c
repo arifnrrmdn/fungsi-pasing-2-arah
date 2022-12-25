b7276cc112ddc5aa0f0a37a41d47afd931889f6b
Twang <iiframdani@gmail.com>
2022-12-25 18:52:43 +0700

    add file

---
+++ Contoh 11.5/Main.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdio.h>
+//deklarasi fungsi
+void tukar(int *a, int *b);
+
+//deklarasi variabel
+int x = 5, y = 10;
+int main()
+{    
+    system("cls");
+    printf("Sebelum ditukar a=%d b=%d\n",x,y);
+    tukar(&x,&y);
+    printf("Setelah ditukar a=%d b=%d",x,y);
+
+    getch();
+    return 0;
+}
+
+//definisi fungsi
+void tukar(int *a, int *b){
+    int temp = 0;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+    printf("Didalam fungsi a=%d b=%d\n",*a,*b);
+}
